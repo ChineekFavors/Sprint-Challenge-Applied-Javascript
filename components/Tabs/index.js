9563d50ff0e7ecb39199162a57803be3ceeb5576
Chineek Favors <chineekfavors01@gmail.com>
2019-07-19 20:38:36 -0700

    working on tabs folder

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,25 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const promise = axios.get('https://lambda-times-backend.herokuapp.com/topics');
+promise
+	.then(response => {	
+		console.log(response.data.topics);
+		const tab = response.data.topics;
+		tab.forEach(tab => tabComponent(tab));	
+	})
+	.catch(error => {
+		console.log ('error:', error);
+	})
+
+function tabComponent(tab){
+
+	const tabs = document.querySelector('.topics');
+	console.log(tab);
+	const div = document.createElement('div');
+	div.classList.add('.tab');
+	div.textContent = tab;
+	
+	return tabs.appendChild(div);
+}
